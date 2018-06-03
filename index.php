92298920bbcfb812335dc51d85192d81763bb0ae
rito-nishino <ritolab@gmail.com>
2018-06-03 09:04:40 +0900

    全体の整形

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 require_once 'autoload.php';
 
 use App\car\Automobile;
+use App\Car\ElectricCar;
+use App\Car\HybridCar;
+use App\Car\HybridCar2;
 
 $automobile = new Automobile();
 $automobile->running();
@@ -10,16 +13,12 @@ $automobile->running();
 
 echo '<br><br>';
 
-use App\Car\ElectricCar;
-
 $electric_car = new ElectricCar();
 $electric_car->running();
 
 // => ガソリン：100 ％
 
 echo '<br><br>';
-
-use App\Car\HybridCar;
 
 $hybrid_car = new HybridCar();
 $hybrid_car->running();
@@ -28,10 +27,7 @@ $hybrid_car->running();
 
 echo '<br><br>';
 
-use App\Car\HybridCar2;
-
 $hybrid_car = new HybridCar2();
 $hybrid_car->running();
 
 // => ガソリン：50 ％ 電気：50 ％
-
